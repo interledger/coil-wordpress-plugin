5c4796ab004abb533ab68525dce8aa20a7da2552
Anthony Hartnell <anthony@pragmatic.agency>
2019-11-15 18:40:26 +0000

    Remove test post types.

--- includes/functions.php
+++ includes/functions.php
@@ -6,8 +6,8 @@ declare(strict_types=1);
 
 namespace Coil;
 
-use \Coil\Gating;
 use \Coil\Admin;
+use \Coil\Gating;
 
 /**
  * Initialise and set up the plugin.
@@ -36,13 +36,9 @@ function init_plugin() : void {
 	add_action( 'admin_menu', __NAMESPACE__ . '\Settings\register_admin_menu' );
 	add_action( 'admin_init', __NAMESPACE__ . '\Settings\maybe_save_coil_admin_settings' );
 	add_action( 'admin_init', __NAMESPACE__ . '\Settings\register_admin_content_settings' );
-	add_filter( 'pre_update_option_coil_content_settings_posts_group', __NAMESPACE__ . '\Settings\maybe_save_coil_content_settings', 10, 3 );
-
-	add_action( 'init', __NAMESPACE__ . '\wpdocs_codex_book_init' );
-	add_action( 'init', __NAMESPACE__ . '\wpdocs_codex_offices_init' );
 
 	// Customizer messaging settings.
-	add_action( 'customize_register',  __NAMESPACE__ . '\Admin\coil_add_customizer_options' );
+	add_action( 'customize_register', __NAMESPACE__ . '\Admin\coil_add_customizer_options' );
 
 	// User profile settings.
 	add_action( 'personal_options', __NAMESPACE__ . '\User\add_user_profile_payment_pointer_option' );
@@ -231,7 +227,7 @@ function print_meta_tag() : void {
  * @return string
  */
 function get_payment_pointer() : string {
-	$coil_status       = Gating\get_post_gating( get_queried_object_id() );
+	$coil_status        = Gating\get_post_gating( get_queried_object_id() );
 	$payment_pointer_id = get_option( 'coil_payout_pointer_id' );
 
 	// If the post is not set for monetising, bail out.
@@ -241,115 +237,3 @@ function get_payment_pointer() : string {
 
 	return $payment_pointer_id;
 }
-
-
-
-
-
-
-
-/**
- * Register a custom post type called "book".
- *
- * @see get_post_type_labels() for label keys.
- */
-function wpdocs_codex_book_init() {
-    $labels = array(
-        'name'                  => _x( 'Books', 'Post type general name', 'textdomain' ),
-        'singular_name'         => _x( 'Book', 'Post type singular name', 'textdomain' ),
-        'menu_name'             => _x( 'Books', 'Admin Menu text', 'textdomain' ),
-        'name_admin_bar'        => _x( 'Book', 'Add New on Toolbar', 'textdomain' ),
-        'add_new'               => __( 'Add New', 'textdomain' ),
-        'add_new_item'          => __( 'Add New Book', 'textdomain' ),
-        'new_item'              => __( 'New Book', 'textdomain' ),
-        'edit_item'             => __( 'Edit Book', 'textdomain' ),
-        'view_item'             => __( 'View Book', 'textdomain' ),
-        'all_items'             => __( 'All Books', 'textdomain' ),
-        'search_items'          => __( 'Search Books', 'textdomain' ),
-        'parent_item_colon'     => __( 'Parent Books:', 'textdomain' ),
-        'not_found'             => __( 'No books found.', 'textdomain' ),
-        'not_found_in_trash'    => __( 'No books found in Trash.', 'textdomain' ),
-        'featured_image'        => _x( 'Book Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'archives'              => _x( 'Book archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
-        'insert_into_item'      => _x( 'Insert into book', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
-        'uploaded_to_this_item' => _x( 'Uploaded to this book', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
-        'filter_items_list'     => _x( 'Filter books list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
-        'items_list_navigation' => _x( 'Books list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
-        'items_list'            => _x( 'Books list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
-    );
-
-    $args = array(
-        'labels'             => $labels,
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'book' ),
-        'capability_type'    => 'post',
-        'has_archive'        => true,
-        'hierarchical'       => false,
-        'menu_position'      => null,
-        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
-    );
-
-    register_post_type( 'book', $args );
-}
-
-
-
-/**
- * Register a custom post type called "book".
- *
- * @see get_post_type_labels() for label keys.
- */
-function wpdocs_codex_offices_init() {
-    $labels = array(
-        'name'                  => _x( 'Offices', 'Post type general name', 'textdomain' ),
-        'singular_name'         => _x( 'Offices', 'Post type singular name', 'textdomain' ),
-        'menu_name'             => _x( 'Offices', 'Admin Menu text', 'textdomain' ),
-        'name_admin_bar'        => _x( 'Book', 'Add New on Toolbar', 'textdomain' ),
-        'add_new'               => __( 'Add New', 'textdomain' ),
-        'add_new_item'          => __( 'Add New Book', 'textdomain' ),
-        'new_item'              => __( 'New Book', 'textdomain' ),
-        'edit_item'             => __( 'Edit Book', 'textdomain' ),
-        'view_item'             => __( 'View Book', 'textdomain' ),
-        'all_items'             => __( 'All Books', 'textdomain' ),
-        'search_items'          => __( 'Search Books', 'textdomain' ),
-        'parent_item_colon'     => __( 'Parent Books:', 'textdomain' ),
-        'not_found'             => __( 'No books found.', 'textdomain' ),
-        'not_found_in_trash'    => __( 'No books found in Trash.', 'textdomain' ),
-        'featured_image'        => _x( 'Book Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'archives'              => _x( 'Book archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
-        'insert_into_item'      => _x( 'Insert into book', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
-        'uploaded_to_this_item' => _x( 'Uploaded to this book', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
-        'filter_items_list'     => _x( 'Filter books list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
-        'items_list_navigation' => _x( 'Books list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
-        'items_list'            => _x( 'Books list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
-    );
-
-    $args = array(
-        'labels'             => $labels,
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'book' ),
-        'capability_type'    => 'post',
-        'has_archive'        => true,
-        'hierarchical'       => false,
-        'menu_position'      => null,
-        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
-    );
-
-    register_post_type( 'offices-test-help_te', $args );
-}
-
-
