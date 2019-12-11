ba2dfef70301d52e96530e24efec436f2d03da9e
Paul Gibbs <pg@pragmatic.agency>
2019-12-11 14:19:30 +0000

    Bump versions to 1.0.6

--- plugin.php
+++ plugin.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 /**
  * Plugin Name: Coil Web Monetization
  * Plugin URI: https://wordpress.org/plugins/coil-web-monetization/
- * Description: Coil offers an effortless way to share content online and get paid for it.
+ * Description: Coil offers an effortless way to share WordPress content online, and get paid for it.
  * Author: Coil
  * Author URI: https://coil.com
- * Version: 1.0.5
+ * Version: 1.0.6
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: coil-web-monetization
@@ -20,7 +20,7 @@ defined( 'ABSPATH' ) || exit;
 /**
  * @var string Plugin version number.
  */
-const PLUGIN_VERSION = '1.0.5';
+const PLUGIN_VERSION = '1.0.6';
 
 require_once __DIR__ . '/includes/admin/functions.php';
 require_once __DIR__ . '/includes/settings/functions.php';
