22bcc599dc19ad4a4fad2b803f25b61efc76ee7c
Paul Gibbs <pg@pragmatic.agency>
2020-06-25 16:28:07 +0100

    Tests: add check for viewing a members-only post without a browser extension.

--- tests/cypress/integration/coil/single-post.js
+++ tests/cypress/integration/coil/single-post.js
@@ -50,9 +50,29 @@ describe('Single Posts', function () {
 
 		cy.get('.hentry .entry-title a')
 			.contains('No Monetization')
-			.click();
+			.then($link => {
+				$link[0].scrollIntoView();
+				$link[0].click();
+			})
 
 		cy.get('.entry-content').should('contain', 'ID: TESTPOST2');
 		cy.get('head meta[name="monetization"]', {timeout: 0}).should('not.exist');
-	} );
+	});
+
+	it('without a browser extension, check that I cannot view a post set to members only.', function() {
+		cy.visit('/');
+
+		cy.get('.hentry .entry-title a')
+			.contains('Members Only')
+			.then($link => {
+				$link[0].scrollIntoView();
+				$link[0].click();
+			})
+
+		// Main article content should be hidden with CSS.
+		cy.get('.entry-content:not(.coil-message-container)').should('not.be.visible');
+
+		// "This content is for members only".
+		cy.get('.coil-message-container').should('be.visible');
+	});
 });
