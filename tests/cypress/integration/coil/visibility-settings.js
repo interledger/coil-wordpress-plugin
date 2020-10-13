1acf910501b6ac527701c631b8011c1c29b2d413
jacksandeman <jack.sandeman@pragmatic.agency>
2020-10-13 15:24:10 +0100

    Added relative URL

--- tests/cypress/integration/coil/visibility-settings.js
+++ tests/cypress/integration/coil/visibility-settings.js
@@ -4,7 +4,7 @@ describe('Tests for visibility settings in editor', () => {
 	})
 
 	it('Checks that visibility settings can be changed in Gutenberg', () => {
-		cy.visit('http://coil-plugin.local/wp-admin/post.php?post=72&action=edit')
+		cy.visit('/wp-admin/post.php?post=72&action=edit')
 
 		cy
 			.get('.interface-complementary-area')
