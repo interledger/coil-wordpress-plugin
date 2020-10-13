5f57794b50725575e374e90c434dd0c987027a9a
Paul Gibbs <pg@pragmatic.agency>
2020-10-13 11:07:32 +0100

    Tests, Cypress: check that the "Get Coil to access" button text can be changed.
    See COI20328-133

---
+++ tests/cypress/integration/coil/messaging-learn-more.js
@@ -0,0 +1,38 @@
+/**
+ * Tests for options under the "Learn more button" Customiser panel.
+ */
+
+describe('"Learn more button" panel', function () {
+	beforeEach(() => {
+		cy.logInToWordPress('admin', 'password');
+	})
+
+	it('checks that the "Get Coil to access" button text can be changed', function() {
+		cy.logInToWordPress('admin', 'password');
+		cy.visit('/wp-admin/customize.php');
+
+		cy.get('h3.accordion-section-title')
+			.contains('Coil Web Monetization')
+			.click()
+		cy.get('h3.accordion-section-title')
+			.contains('Learn more button')
+			.click();
+
+		var label = 'New Button Text ' + Date.now();
+		cy.get('#\_customize-input-coil_learn_more_button_text').clear().type(label);
+		cy.get('#save').click();
+
+		cy.reload();
+
+		cy.get('h3.accordion-section-title')
+			.contains('Coil Web Monetization')
+			.click()
+		cy.get('h3.accordion-section-title')
+			.contains('Learn more button')
+			.click();
+
+		cy.get('#\_customize-input-coil_learn_more_button_text').should('have.value', label);
+	});
+});
+
+
