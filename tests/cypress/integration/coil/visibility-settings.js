93b11be415311555a5b63d240fdf0cb70cd164dc
jacksandeman <jack.sandeman@pragmatic.agency>
2020-10-13 15:42:52 +0100

    Added clarification that it is testing against the post not a block

--- tests/cypress/integration/coil/visibility-settings.js
+++ tests/cypress/integration/coil/visibility-settings.js
@@ -3,7 +3,7 @@ describe('Tests for visibility settings in editor', () => {
 		cy.logInToWordPress('admin', 'password');
 	})
 
-	it('Checks that visibility settings can be changed in Gutenberg', () => {
+	it('Checks that visibility settings of a post can be changed in Gutenberg', () => {
 		cy.visit('/wp-admin/post.php?post=72&action=edit')
 
 		cy
