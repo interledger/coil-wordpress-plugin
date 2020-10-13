d3008395981e0ea37f84be8cd36449444b7595ee
jacksandeman <jack.sandeman@pragmatic.agency>
2020-10-13 14:36:47 +0100

    Added test for visibility settings

---
+++ tests/cypress/integration/coil/visibility-settings.js
@@ -0,0 +1,54 @@
+describe('Tests for visibility settings in editor', () => {
+	beforeEach(() => {
+		cy.logInToWordPress('admin', 'password');
+	})
+
+	it('Checks that visibility settings can be changed in Gutenberg', () => {
+		cy.visit('http://coil-plugin.local/wp-admin/post.php?post=72&action=edit')
+
+		cy
+			.get('.interface-complementary-area')
+			.contains('Coil Web Monetization')
+			.click({force: true});
+
+		cy
+			.get('.components-radio-control__input')
+			.then(options => {
+				if(options[0].checked) {
+					return options[0];
+				} else {
+					return options[1];
+				}
+			})
+			.next()
+			.invoke('text')
+			.as('checkedOptionText')
+
+		cy
+			.get('.components-radio-control__input')
+			.then(options => {
+				if(options[0].checked) {
+					return options[1];
+				} else {
+					return options[0];
+				}
+			})
+			.click()
+
+		cy
+			.get('.editor-post-publish-button')
+			.click();
+
+		cy.reload();
+
+		cy
+			.get('@checkedOptionText')
+			.then((checkedOptionText) => {
+				cy
+					.contains(checkedOptionText)
+					.prev()
+					.should('not.be.checked')
+			})
+
+	})
+})
