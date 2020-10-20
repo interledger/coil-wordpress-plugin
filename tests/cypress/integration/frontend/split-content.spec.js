7c115d4e699fb54e01a8eb4bf452c6734aedd391
jacksandeman <jack.sandeman@pragmatic.agency>
2020-10-20 11:36:34 +0100

    Introduced variable for hidden content message Updated test wording

--- tests/cypress/integration/frontend/split-content.spec.js
+++ tests/cypress/integration/frontend/split-content.spec.js
@@ -1,37 +1,39 @@
-describe('Block visibility tests for non WM-enabled users', () => {
+const hiddenContentMessage = 'This content is for Coil Members only. To access, join Coil and install the browser extension.';
+
+describe('Visibility of content blocks for non WM-enabled users', () => {
 	beforeEach(() => {
 		cy.visit('/block-visibility/')
 	})
 
-	it('Checks block visibility settings for blocks hidden to non WM-enabled users', () => {
+	it('Check visibility of content blocks hidden to non WM-enabled users', () => {
 		cy
 			.get('.wm-shown h3')
 			.invoke('text')
-			.should('contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('contain', hiddenContentMessage)
 
 		cy
 			.get('.wm-shown p')
 			.invoke('text')
-			.should('contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('contain', hiddenContentMessage)
 	})
 
-	it('Checks block visibility settings for blocks shown to non WM-enabled users', () => {
+	it('Check visibility of content blocks shown to non WM-enabled users', () => {
 		cy
 			.get('.everyone-shown h3')
 			.invoke('text')
-			.should('not.contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('not.contain', hiddenContentMessage)
 
 		cy
 			.get('.everyone-shown img')
 			.invoke('text')
-			.should('not.contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('not.contain', hiddenContentMessage)
 	})
 
-	it('Checks block visibility settings for blocks hidden from WM-enabled users', () => {
+	it('Check visibility of content blocks hidden from WM-enabled users', () => {
 		cy
 			.get('.wm-hidden h3')
 			.invoke('text')
-			.should('not.contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('not.contain', hiddenContentMessage)
 
 		cy
 			.get('.wm-hidden ')
@@ -39,7 +41,7 @@ describe('Block visibility tests for non WM-enabled users', () => {
 	})
 })
 
-describe('Block visibility tests for WM-enabled users', () => {
+describe('Check visibility of content blocks for WM-enabled users', () => {
 	beforeEach(() => {
 		cy.visit('/block-visibility/')
 		cy.startWebMonetization();
@@ -49,31 +51,31 @@ describe('Block visibility tests for WM-enabled users', () => {
 		cy.stopWebMonetization();
 	})
 
-	it('Checks block visibility settings for blocks hidden to non WM-enabled users', () => {
+	it('Check visibility of content blocks hidden to non WM-enabled users', () => {
 		cy
 			.get('.wm-shown h3')
 			.invoke('text')
-			.should('not.contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('not.contain', hiddenContentMessage)
 
 		cy
 			.get('.wm-shown p')
 			.invoke('text')
-			.should('not.contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('not.contain', hiddenContentMessage)
 	})
 
-	it('Checks block visibility settings for blocks shown to non WM-enabled users', () => {
+	it('Check visibility of content blocks shown to non WM-enabled users', () => {
 		cy
 			.get('.everyone-shown h3')
 			.invoke('text')
-			.should('not.contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('not.contain', hiddenContentMessage)
 
 		cy
 			.get('.everyone-shown img')
 			.invoke('text')
-			.should('not.contain', 'This content is for Coil Members only. To access, join Coil and install the browser extension.')
+			.should('not.contain', hiddenContentMessage)
 	})
 
-	it('Checks block visibility settings for blocks hidden from WM-enabled users', () => {
+	it('Check visibility of content blocks hidden from WM-enabled users', () => {
 		cy
 			.get('.wm-hidden')
 			.should('not.be.visible')
