659198282dd3c338ef8ca61e6dbf24a4f09de052
SarahCoilAccount <sarah@coil.com>
2021-03-08 15:19:31 +0200

    Fixed double inclusion of target="_blank"

--- includes/settings/functions.php
+++ includes/settings/functions.php
@@ -272,7 +272,7 @@ function coil_global_settings_payment_pointer_render_callback() {
 	echo $payment_pointer_description . '</p>'; // phpcs:ignore. Output already escaped.
 
 	printf(
-		'<br><a target="_blank" href="%s" target="%s" class="%s">%s</a>',
+		'<br><a href="%s" target="%s" class="%s">%s</a>',
 		esc_url( 'https://webmonetization.org/docs/ilp-wallets' ),
 		esc_attr( '_blank' ),
 		esc_attr( 'button button-large' ),
