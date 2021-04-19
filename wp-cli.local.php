8a01615181857e23229cda5ee6ba1818d788b7e8
SarahCoilAccount <sarah@coil.com>
2021-04-19 11:58:03 +0200

    Removed wp cli files from directory

--- wp-cli.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-define( 'DB_HOST', '192.168.95.100:10003' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
-
-// Only display fatal run-time errors.
-// See http://php.net/manual/en/errorfunc.constants.php.
-error_reporting( 1 );
-define( 'WP_DEBUG', false );
