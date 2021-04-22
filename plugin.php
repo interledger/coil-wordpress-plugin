4569bd1877930f033ee0963ef6b3b353df4ce0b2
Marc Perel <marc@obox.co.za>
2021-04-22 11:51:59 +0200

    Updated version number

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Requires PHP: 7.2
  * Author: Coil
  * Author URI: https://coil.com
- * Version: 1.8.0
+ * Version: 1.9.0
  * License: Apache-2.0
  * License URI: http://www.apache.org/licenses/LICENSE-2.0.txt
  * Text Domain: coil-web-monetization
