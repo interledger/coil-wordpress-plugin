5e005524d44aeb92ea234908feaa58445118124c
SarahCoilAccount <sarah@coil.com>
2021-05-12 16:23:32 +0200

    Removed error logs

--- includes/settings/functions.php
+++ includes/settings/functions.php
@@ -1028,8 +1028,6 @@ function transfer_customizer_monetization_settings() {
 
 	$previous_gating_options = get_option( 'coil_content_settings_posts_group' );
 
-	error_log( 'existing options: ' . json_encode( $existing_options ) );
-
 	// If the setting has already been saved or transferred then simply return
 	if ( get_option( 'coil_monetization_settings_group' ) || ( ! get_option( 'coil_content_settings_posts_group' ) && ! get_theme_mod( 'coil_title_padlock' ) && ! get_theme_mod( 'coil_show_donation_bar' ) ) ) {
 		return;
@@ -1057,7 +1055,6 @@ function transfer_customizer_monetization_settings() {
 			}
 		}
 	}
-	error_log( '$new_monetization_settings: ' . json_encode( $new_monetization_settings ) );
 
 	// Delete old row
 	delete_option( 'coil_content_settings_posts_group' );
