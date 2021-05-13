2f0b86eb0d87501795975c7956e2c2fbb58ed924
SarahCoilAccount <sarah@coil.com>
2021-05-13 13:06:01 +0200

    New return condition

--- includes/settings/functions.php
+++ includes/settings/functions.php
@@ -1029,7 +1029,7 @@ function transfer_customizer_monetization_settings() {
 	$previous_gating_options = get_option( 'coil_content_settings_posts_group' );
 
 	// If the setting has already been saved or transferred then simply return
-	if ( get_option( 'coil_monetization_settings_group' ) || ( ! get_option( 'coil_content_settings_posts_group' ) && ! get_theme_mod( 'coil_title_padlock' ) && ! get_theme_mod( 'coil_show_donation_bar' ) ) ) {
+	if ( get_option( 'coil_monetization_settings_group' ) || ( ! get_option( 'coil_content_settings_posts_group' ) && 'null' !== get_theme_mod( 'coil_title_padlock', 'null' ) && 'null' !== get_theme_mod( 'coil_show_donation_bar', 'null' ) ) ) {
 		return;
 	}
 
