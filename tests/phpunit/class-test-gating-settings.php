441f6fbeca41bb72f546a5a864f4aa70e716da12
SarahCoilAccount <sarah@coil.com>
2021-06-04 15:00:14 +0200

    Adding content gating tests

--- tests/phpunit/class-test-gating-settings.php
+++ tests/phpunit/class-test-gating-settings.php
@@ -122,17 +122,53 @@ class Test_Gating_Settings extends WP_UnitTestCase {
 		}
 	}
 
+	/**
+	 * Tests if the correct global default monetization settings can be correctly retrieved.
+	 *
+	 * @return void
+	 */
+	public function test_retrieving_default_global_monetization_settings() :  void {
+
+		$monetization_options = [
+			[
+				'post' => 'no',
+				'page' => 'no',
+			],
+			[
+				'post' => 'no-gating',
+				'page' => 'no-gating',
+			],
+			[
+				'post' => 'gate-all',
+				'page' => 'gate-all',
+			],
+		];
+
+		foreach ( $monetization_options as $global_gating ) {
+			update_option( 'coil_monetization_settings_group', $global_gating );
+			$this->assertSame( $global_gating, Gating\get_global_posts_gating() );
+		}
+	}
+
 	// /**
-	//  * Tests taxonomy gating.
+	//  * Tests if the correct gating value retrieved for a specific post is returned 
+	//  * in cases that consider the combination of global, taxonomy or post monetization settings that have been used.
 	//  *
 	//  * @return void
 	//  */
-	// public function test_taxonomy_term_gating() :  void {
+	// public function test_retrieving_content_gating() :  void {
 
-	// 	// add global defaults to the database
-	// 	update_option( 'coil_monetization_settings_group' );
-	// 	// create array of expected values $global_gating =
-	// 	printf( Gating\get_global_posts_gating() );
-	// 	//$this->assertSame( $global_gating, Gating\et_global_posts_gating() );
+	// 	$post_obj = self::factory()->post->create_and_get();
+
+	// 	$monetization_settings = [ 'post' => 'gate-all', 'page' => 'gate-all' ];
+	// 	update_option( 'coil_monetization_settings_group', $monetization_settings ); 
+
+	// 	$this->assertSame( $global_gating, Gating\get_content_gating() );
+	// 	update_option( 'coil_monetization_settings_group', $monetization_settings );
+	// 	update_option( 'coil_monetization_settings_group', $monetization_settings );
+	// 	//Test with global
+	// 	// Test will taxonomy
+	// 	// Test with post level
+
 	// }
 }
